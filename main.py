769008f867ea8e3e1240bda25e73872976ca0a68
iamshiva003 <shivu9887@gmail.com>
2023-07-19 10:16:56 +0530

    Revert "adding names"
    This reverts commit b1e2fa801daa11711b9dd67c40dc3455e31b3196.

--- main.py
+++ main.py
@@ -2,12 +2,3 @@ print("Hello world")
 
 for letter in "Shivu":
     print(letter)
-
-
-def names(name):
-    length = len(name)
-    print(f"The length of the string is : {length}")
-
-
-names("shivu")
-names("Kempya")
